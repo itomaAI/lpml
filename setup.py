0312ae6a047b51f541f3ed1f68915890c72ec688
ryamauchi <ryutaro.yamauchi@itoma.ai>
2025-05-02 12:39:46 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='lpml',
-    version='0.1.0',
+    version='0.0.0',
     author='itomaAI inc.',
-    author_email='info@itomaai.com',
+    author_email='ryutaro.yamauchi@itoma.ai',
     description='LPML: LLM Prompting Markup Language',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/itomaAI/lpml',  # GitHubリポジトリのURLに置き換えてください
-    packages=find_packages(),
+    url='https://github.com/itomaAI/lpml',
+    packages=['lpml'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.12',
+    python_requires='>=3.11',
 )
